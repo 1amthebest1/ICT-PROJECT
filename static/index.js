32d0137c59d69f040f59f1a63af22efdac0380a8
Baker <voiceee54@gmail.com>
2025-05-25 16:09:48 -0400

    fixed bugs

--- static/index.js
+++ static/index.js
@@ -1,6 +1,16 @@
-// function changeLocation(image){
-//     window.location="";
-// }
+function changeLocation(image){
+    let x = document.getElementsByClassName(image)[0].href;
+    let id = x.indexOf("?");
+    let ID=image.slice(3);
+    if(id==-1){
+    let imageToPass=x.slice(39);
+    window.location="http://shaban.com:8000/phpFiles/buy.php?id="+ID+"&fileToRead="+imageToPass;
+    }
+    else{
+    let imageToPass=x.slice(39, id);
+    window.location="http://shaban.com:8000/phpFiles/buy.php?id="+ID+"&fileToRead="+imageToPass;
+    }
+}
 
 function viewContent(image){
     // let image=document.getElementById(image);
